c0ab11d2e7fa733b7c512bf43ffce496af71734f
tejasreeaalla <tejasree.aalla@gmail.com>
2025-11-08 03:24:39 -0500

    Delete test_request.py

--- test_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.post(
-    "http://127.0.0.1:5000/ai-process",
-    json={"command": "Make a note that I need to buy ice cream!"}
-)
-
-print("Status Code:", response.status_code)
-print("Response Body:", response.text)
